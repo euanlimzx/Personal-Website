71b7b026f4e4bd6df1ce5dc220237ea30a7f0855
euanlimzx <euanlimzx@gmail.com>
2022-09-09 17:30:16 +0800

    javascript undone

---
+++ website/static/website/projectjc.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded',function(){
+    //for the euanlimzx homepage
+    document.querySelector('.bi-instagram').addEventListener('click',()=>gdocs());
+    document.querySelector('.bi-github').addEventListener('click',()=>tiktok());
+
+})
+
+function telegram(){
+    location.href="https://t.me/euanlim"
+}
+
+function instagram(){
+    location.href="https://www.instagram.com/euanlim_/"
+}
+
+function github(){
+    location.href="https://github.com/euanlimzx?tab=repositories"
+}
